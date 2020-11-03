9cdce321bede74a7d5e4645d3d29bfaeedc7c720
tejumdev <73911123+tejumdev@users.noreply.github.com>
2020-11-03 17:29:13 -0600

    Update anagramMapping.java

--- anagramMapping.java
+++ anagramMapping.java
@@ -1,5 +1,10 @@
 package practise;
 
+/* Given two lists Aand B, and B is an anagram of A. B is an anagram of A means B is made by randomizing the order of the elements in A.
+
+We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
+
+These lists A and B may contain duplicates. If there are multiple answers, output any of them. */
 
 import java.util.stream.IntStream;
 
