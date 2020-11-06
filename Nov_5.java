7939932a64f9411b2d9f245011b9a31f1fc220e2
tejumdev <73911123+tejumdev@users.noreply.github.com>
2020-11-05 21:11:43 -0600

    Update Nov_5.java

--- Nov_5.java
+++ Nov_5.java
@@ -12,3 +12,22 @@
        }
         return even_digits;
     }
+
+   public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int arr_len = arr.length;
+    	long count = IntStream.range(0,arr_len)
+	.filter(currIndex -> {
+		int j=currIndex+1;
+		int k=currIndex+2;
+		if(currIndex <= arr_len - 3) {
+//             System.out.println(Math.abs(arr[currIndex] - arr[j]) +" "+Math.abs(arr[j] - arr[k])+" "+Math.abs(arr[currIndex] - arr[k]) );
+			if((Math.abs(arr[currIndex] - arr[j]) <= a)& (Math.abs(arr[j] - arr[k]) <= b)& (Math.abs(arr[currIndex] - arr[k]) <= c)) {return true;}	
+			else return false;
+		}
+		else return false;
+		
+		}).count();
+	
+	// System.out.println(count);
+        return (int) count;
+    }
